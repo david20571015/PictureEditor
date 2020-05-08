c001c9956f5f724749fa5678adcbf201d0e2f302
david <david20571015@gmail.com>
2020-05-09 00:42:03 +0800

    refacotr: change filechooser

--- PictureViewerController.java
+++ PictureViewerController.java
@@ -1,16 +1,14 @@
 import java.io.File;
 
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
+import javafx.stage.DirectoryChooser;
 
 public class PictureViewerController {
     private File currentFolderPath;
+    private String defaultOpenFolderPath = "D:/PxDownloader";// System.getProperty("user.home") + "/Desktop"
 
     @FXML
     private MenuItem openMenuItem;
@@ -22,27 +20,15 @@ public class PictureViewerController {
     }
 
     @FXML
-    void openMenuItemOnAction(ActionEvent event) {
-        currentFolderPath = getFolderPath();
-        folderTree.setText(currentFolderPath.toString());
-    }
+    void openMenuItemOnAction(final ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open Folder");
+        directoryChooser.setInitialDirectory(new File(defaultOpenFolderPath));
+        currentFolderPath = directoryChooser.showDialog(null);
 
-    private File getFolderPath() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-        }
+        String[] parsedPath = currentFolderPath.toString().split("\\\\");
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-        if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION)
-            return chooser.getSelectedFile();
-        else
-            return null;
+        folderTree.setText(parsedPath[parsedPath.length - 1]);
     }
 
 }
