5a2568cfc49da7e20a187776fbf56920df1e8230
jacky0303 <55957310+jacky0303@users.noreply.github.com>
2020-05-09 00:01:14 +0800

    image

--- PictureViewerController.java
+++ PictureViewerController.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -8,17 +11,37 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 
 public class PictureViewerController {
     private File currentFolderPath;
 
-    @FXML
-    private MenuItem openMenuItem;
+    @FXML private MenuItem openMenuItem;
 
-    @FXML
-    private TitledPane folderTree;
+    @FXML private TitledPane folderTree;
+    
+    @FXML private ImageView imageView;
+
 
     public void initialize() {
+    }
+
+    @FXML
+    void imageViewDragOver(DragEvent event) {
+        if (event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
+    @FXML
+    void imageViewDropped(DragEvent event) throws FileNotFoundException{
+        List<File> file = event.getDragboard().getFiles();
+        Image img = new Image(new FileInputStream(file.get(0)));
+        imageView.setImage(img);
+
     }
 
     @FXML
