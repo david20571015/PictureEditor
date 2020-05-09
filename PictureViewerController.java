f9194c253eb6c53f892570b43420f8e00805fa31
jacky0303 <55957310+jacky0303@users.noreply.github.com>
2020-05-09 17:00:02 +0800

    20200509

--- PictureViewerController.java
+++ PictureViewerController.java
@@ -3,31 +3,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
+import javafx.stage.DirectoryChooser;
 
 public class PictureViewerController {
     private File currentFolderPath;
+    private String defaultOpenFolderPath = "D:/HOMEWORK/2nd_Spring";// System.getProperty("user.home") + "/Desktop"
 
-    @FXML private MenuItem openMenuItem;
+    @FXML
+    private MenuItem openMenuItem;
 
-    @FXML private TitledPane folderTree;
-    
-    @FXML private ImageView imageView;
+    @FXML
+    private TitledPane folderTitledPane;
 
+    @FXML
+    private ImageView imageView;
 
-    public void initialize() {
-    }
+    @FXML
+    private TreeView<File> folderTreeView;
 
     @FXML
     void imageViewDragOver(DragEvent event) {
@@ -37,34 +41,72 @@ public class PictureViewerController {
     }
 
     @FXML
-    void imageViewDropped(DragEvent event) throws FileNotFoundException{
+    void imageViewDropped(DragEvent event) throws FileNotFoundException {
         List<File> file = event.getDragboard().getFiles();
         Image img = new Image(new FileInputStream(file.get(0)));
         imageView.setImage(img);
+
     }
 
     @FXML
-    void openMenuItemOnAction(ActionEvent event) {
-        currentFolderPath = getFolderPath();
-        folderTree.setText(currentFolderPath.toString());
+    void openMenuItemOnAction(final ActionEvent event) {
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open Folder");
+        directoryChooser.setInitialDirectory(new File(defaultOpenFolderPath));
+        currentFolderPath = directoryChooser.showDialog(null);
+
+        String[] parsedPath = currentFolderPath.toString().split("\\\\");
+
+        folderTitledPane.setText(parsedPath[parsedPath.length - 1]);
+
+        folderTreeView = new TreeView<File>(new FolderItem(currentFolderPath));
+        folderTitledPane.setContent(folderTreeView);
+
     }
 
-    private File getFolderPath() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
+}
+
+class FolderItem extends TreeItem<File> {
+    private boolean isFirstTimeChildren = true;
+    private boolean isFirstTimeLeaf = true;
+    private boolean isLeaf;
+
+    public FolderItem(File rootPath) {
+        super(rootPath);
+    }
+
+    @Override
+    public ObservableList<TreeItem<File>> getChildren() {
+        if (isFirstTimeChildren) {
+            isFirstTimeChildren = false;
+            super.getChildren().setAll(buildChildren(this));
         }
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-        if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION)
-            return chooser.getSelectedFile();
-        else
-            return null;
+        return super.getChildren();
     }
 
+    @Override
+    public boolean isLeaf() {
+        if (isFirstTimeLeaf) {
+            isFirstTimeLeaf = false;
+            File f = (File) getValue();
+            isLeaf = f.isFile();
+        }
+        return isLeaf;
+    }
+
+    private ObservableList<TreeItem<File>> buildChildren(TreeItem<File> treeItem) {
+        File f = treeItem.getValue();
+
+        if (f != null && f.isDirectory()) {
+            File[] files = f.listFiles();
+            if (files != null) {
+                ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
+                for (File childrenFile : files)
+                    if (childrenFile.isDirectory())
+                        children.add(new FolderItem(childrenFile));
+                return children;
+            }
+        }
+        return FXCollections.emptyObservableList();
+    }
 }
