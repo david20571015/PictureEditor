361ae25bf9318af9f6ae4f4b55421b210b05f6da
david <david20571015@gmail.com>
2020-05-21 18:12:00 +0800

    tmp

--- src/operation/imageoperation/ImageOperation.java
+++ src/operation/imageoperation/ImageOperation.java
@@ -3,7 +3,19 @@ package src.operation.imageoperation;
 import src.operation.Operation;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
-public interface ImageOperation extends Operation {
-    Image execute(Image img);
+public abstract class ImageOperation implements Operation {
+    public abstract WritableImage execute(Image img);
+
+    private WritableImage conv2d(Image inputImage, double[][] mask) {
+        WritableImage input = new WritableImage(inputImage.getPixelReader(), (int) inputImage.getWidth(),
+                (int) inputImage.getHeight());
+
+    }
+
+    private Color[][] imageToMatrix(Image inputImage, Color padding) {
+
+    }
 }
