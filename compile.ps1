b0812f40e662ca1b97fb9c10c3c2d74dcdb087ce
david <david20571015@gmail.com>
2020-07-01 00:29:16 +0800

    fix : compile command

--- compile.ps1
+++ compile.ps1
@@ -1,3 +1,2 @@
-javac --module-path=$env:PATH_TO_FX -encoding UTF-8 --add-modules=javafx.fxml,javafx.swing,javafx.controls -cp "metadata-extractor-2.14.0.jar;." .\src\resource\*.java .\src\window\*.java .\src\*.java 
-java --module-path=$env:PATH_TO_FX --add-modules=javafx.fxml,javafx.swing,javafx.controls -cp "metadata-extractor-2.14.0.jar;.;.\src\resource\;.\src\window\;.\src\" src.PictureViewer
-
+javac --module-path=$env:PATH_TO_FX -encoding UTF-8 --add-modules=javafx.fxml,javafx.swing,javafx.controls -cp "metadata-extractor-2.14.0.jar;." .\src\window\*.java .\src\controller\*.java  .\src\*.java 
+java --module-path=$env:PATH_TO_FX --add-modules=javafx.fxml,javafx.swing,javafx.controls -cp "metadata-extractor-2.14.0.jar;.;.\src\window\;.\src\controller\;.\src\" src.PictureViewer
